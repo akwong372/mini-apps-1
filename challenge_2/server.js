8e00e8eef219252715758106494ab9aeec1e572c
akwong372 <akwong372@gmail.com>
2019-01-08 12:54:28 -0800

    Create basic server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,20 @@
+// Use Express to serve up an index.html file and its associated assets
 
+// The client app should be able to submit JSON data to the server,
+// receive a response containing a CSV-formatted result
+// (the way this is done will vary depending on where you are in the challenge).
 
+// Implement all the report generation logic on the server.
+// Do not use any external libraries (such as via npm). You may use jQuery on the client.
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('client'))
+
+app.get('/', (req, res) => {
+  res.send('test');
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`))
