6fbb7fa3f149be1e036e5ddae826807c8688928b
akwong372 <akwong372@gmail.com>
2019-01-12 12:27:34 -0800

    React working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,20 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 1
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>A React Component</h1>
+        <h2>test state {this.state.test}</h2>
+      </div>
+    )
+  }
 
+}
+
+window.App = App;
