489bbdd76e63d57504e3ff5bdeccaf7c63f2710e
akwong372 <akwong372@gmail.com>
2019-01-13 15:10:10 -0800

    Connect to mongodb server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert');
 const app = express();
 const port = 3000;
+var url = 'mongodb://localhost:27017/miniAppChallenge3';
 
 
 app.use(express.static('public'))
+app.use(bodyParser.json());
+
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
 
 app.get('/', (req, res) => {
   res.send('test');
