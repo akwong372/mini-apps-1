a79a19376b55ef7dd204615ac406d578eb2d694a
akwong372 <akwong372@gmail.com>
2019-01-16 15:47:26 -0800

    Allow to drop pieces by clicking column

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -4,74 +4,109 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPlayer: '[R]',
+      currentPlayer: '',
     }
     this.placePiece = this.placePiece.bind(this)
   }
 
-placePiece(e) {
-  console.log(e.target.textContent)
-  e.target.textContent = this.state.currentPlayer;
-}
+  switchPlayer() {
+    if (this.state.currentPlayer === '[R]') {
+      this.setState({
+        currentPlayer: '[Y]'
+      });
+    } else {
+      this.setState({
+        currentPlayer: '[R]'
+      });
+    }
+  }
+
+  dropPiece(e) {
+    console.log('X: ',e.target.attributes.x);
+    console.log('Y: ',e.target.attributes.y);
+    console.log(e.target.parentNode.children);
+    var column = e.target.parentNode.children;
+    for (var i = column.length-1; i > 0; i--){
+      if (column[i].textContent === '[  ]'){
+        this.placePiece(column[i]);
+        break;
+      }
+    }
+  }
+
+  placePiece(space) {
+    if (space.textContent !== '[R]' && space.textContent !== '[Y]') {
+      space.textContent = this.state.currentPlayer;
+      this.switchPlayer();
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentPlayer: '[R]'
+    })
+  }
 
   render() {
     return (
-      <div className='container'>
-        <h2>game board</h2>
-        <div id='row5' className='row'>
-          <Square x={0} y={5} placePiece={this.placePiece}/>
-          <Square x={1} y={5} placePiece={this.placePiece}/>
-          <Square x={2} y={5} placePiece={this.placePiece}/>
-          <Square x={3} y={5} placePiece={this.placePiece}/>
-          <Square x={4} y={5} placePiece={this.placePiece}/>
-          <Square x={5} y={5} placePiece={this.placePiece}/>
-          <Square x={6} y={5} placePiece={this.placePiece}/>
+      <div className='container row' onClick={(e) => {this.dropPiece(e)}}>
+        <div id='column0'>
+          <Square y={5} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={4} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={3} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={2} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={1} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={0} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
         </div>
-        <div id='row4' className='row'>
-          <Square x={0} y={4} placePiece={this.placePiece}/>
-          <Square x={1} y={4} placePiece={this.placePiece}/>
-          <Square x={2} y={4} placePiece={this.placePiece}/>
-          <Square x={3} y={4} placePiece={this.placePiece}/>
-          <Square x={4} y={4} placePiece={this.placePiece}/>
-          <Square x={5} y={4} placePiece={this.placePiece}/>
-          <Square x={6} y={4} placePiece={this.placePiece}/>
+        <div id='column1'>
+          <Square y={5} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={4} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={3} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={2} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={1} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={0} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
         </div>
-        <div id='row3' className='row'>
-          <Square x={0} y={3} placePiece={this.placePiece}/>
-          <Square x={1} y={3} placePiece={this.placePiece}/>
-          <Square x={2} y={3} placePiece={this.placePiece}/>
-          <Square x={3} y={3} placePiece={this.placePiece}/>
-          <Square x={4} y={3} placePiece={this.placePiece}/>
-          <Square x={5} y={3} placePiece={this.placePiece}/>
-          <Square x={6} y={3} placePiece={this.placePiece}/>
+        <div id='column2'>
+          <Square y={5} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={4} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={3} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={2} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={1} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={0} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
         </div>
-        <div id='row2' className='row'>
-          <Square x={0} y={2} placePiece={this.placePiece}/>
-          <Square x={1} y={2} placePiece={this.placePiece}/>
-          <Square x={2} y={2} placePiece={this.placePiece}/>
-          <Square x={3} y={2} placePiece={this.placePiece}/>
-          <Square x={4} y={2} placePiece={this.placePiece}/>
-          <Square x={5} y={2} placePiece={this.placePiece}/>
-          <Square x={6} y={2} placePiece={this.placePiece}/>
+        <div id='column3'>
+          <Square y={5} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={4} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={3} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={2} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={1} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={0} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
         </div>
-        <div id='row1' className='row'>
-          <Square x={0} y={1} placePiece={this.placePiece}/>
-          <Square x={1} y={1} placePiece={this.placePiece}/>
-          <Square x={2} y={1} placePiece={this.placePiece}/>
-          <Square x={3} y={1} placePiece={this.placePiece}/>
-          <Square x={4} y={1} placePiece={this.placePiece}/>
-          <Square x={5} y={1} placePiece={this.placePiece}/>
-          <Square x={6} y={1} placePiece={this.placePiece}/>
+        <div id='column4'>
+          <Square y={5} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={4} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={3} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={2} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={1} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={0} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
         </div>
-        <div id='row0' className='row'>
-          <Square x={0} y={0} placePiece={this.placePiece}/>
-          <Square x={1} y={0} placePiece={this.placePiece}/>
-          <Square x={2} y={0} placePiece={this.placePiece}/>
-          <Square x={3} y={0} placePiece={this.placePiece}/>
-          <Square x={4} y={0} placePiece={this.placePiece}/>
-          <Square x={5} y={0} placePiece={this.placePiece}/>
-          <Square x={6} y={0} placePiece={this.placePiece}/>
+        <div id='column5'>
+          <Square y={5} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={4} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={3} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={2} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={1} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={0} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
         </div>
+        <div id='column6'>
+          <Square y={5} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={4} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={3} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={2} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={1} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          <Square y={0} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+        </div>
+
       </div>
     )
   }
--- challenge_4/client/src/components/square.jsx
+++ challenge_4/client/src/components/square.jsx
@@ -2,14 +2,35 @@ class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      red: 0,
-      yellow: 0
+      clicked: 0
     }
   }
 
+  // detectPlayer() {
+  //   console.log(this)
+  //   if (this.props.currentPlayer === '[R]' && this.state.red === 0 && this.state.yellow === 0){
+  //     this.setState({
+  //       red: 1,
+  //       yellow: 0
+  //     });
+  //   } else {
+  //     this.setState({
+  //       red: 0,
+  //       yellow: 1
+  //     });
+  //   }
+  //   console.log(this.state)
+  // }
+
+  // clicked(){
+  //   this.setState({
+  //     clicked: 1
+  //   }, ()=>console.log(this.state))
+  // }
+
   render() {
     return (
-      <div className='square' onClick={(e)=>{e.persist(); this.props.placePiece(e)}}>
+      <div x={this.props.x} y={this.props.y} className='square' onClick={(e)=>{}}>
         [  ]
       </div>
     )
