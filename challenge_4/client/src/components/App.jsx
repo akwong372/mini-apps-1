bdc0c696c76923df92f1abc21d60b275b646bd59
akwong372 <akwong372@gmail.com>
2019-01-16 23:07:50 -0800

    Detect win for horizontal and vertical

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -25,10 +25,7 @@ class App extends React.Component {
     }
   }
 
-  dropPiece(e) { //place pieces starting from bottom
-    // console.log('X: ',e.target.attributes.x);
-    // console.log('Y: ',e.target.attributes.y);
-    // console.log(e.target.parentNode.children);
+  dropPiece(e) {
     var column = e.target.parentNode.children;
     for (var i = column.length - 1; i >= 0; i--) {
       if (column[i].textContent === '[  ]') {
@@ -45,11 +42,11 @@ class App extends React.Component {
       if (this.state.currentPlayer === '[R]') {
         this.setState({
           redSpaces: [...this.state.redSpaces, [xCoord, yCoord]]
-        }, ()=>this.checkColumnWin())
+        }, () => this.checkWin())
       } else if (this.state.currentPlayer === '[Y]') {
         this.setState({
           yellowSpaces: [...this.state.yellowSpaces, [xCoord, yCoord]]
-        }, ()=>this.checkColumnWin())
+        }, () => this.checkWin())
       }
       space.textContent = this.state.currentPlayer;
       this.switchPlayer();
@@ -60,22 +57,26 @@ class App extends React.Component {
     var currColumn = [];
     for (var i = 0; i < this.state.xCoords.length; i++) {
       if (this.state.currentPlayer === '[Y]') {
-        currColumn = this.state.redSpaces.map((pair) => {
+        currColumn = this.state.redSpaces.map((pair) => {//collect only y coordinates of each used space of each column
           if (pair[0] === i) {
             return pair[1];
           }
-        }).filter((item) => item !== undefined);
+        })
+          .filter((item) => item !== undefined)//filter out undefined
+          .sort((a, b) => a - b);//sort smallest to largest
       } else if (this.state.currentPlayer === '[R]') {
         currColumn = this.state.yellowSpaces.map((pair) => {
           if (pair[0] === i) {
             return pair[1];
           }
-        }).filter((item) => item !== undefined);
+        })
+          .filter((item) => item !== undefined)
+          .sort((a, b) => a - b);
       }
-      console.log(`column ${i}: `, currColumn);
-      if (currColumn.length>3){
-        for (var i = 0; i < 2; i++){
-          if ((currColumn[i] || currColumn[i] === 0) && currColumn[i+1] && currColumn[i+2] && currColumn[i+3]){
+      //console.log(`column ${i}: `, currColumn);
+      if (currColumn.length > 3) {
+        for (var i = 0; i < 2; i++) {
+          if ((currColumn[i] && currColumn.includes(currColumn[i] + 1) && currColumn.includes(currColumn[i] + 2) && currColumn.includes(currColumn[i] + 3)) || (currColumn[i] === 0 && currColumn[1] === 1 && currColumn[2] === 2 && currColumn[3] === 3)) {
             console.log('win')
           }
         }
@@ -83,6 +84,45 @@ class App extends React.Component {
       }
     }
   }
+
+  checkRowWin() {
+    var currRow = [];
+    for (var i = 0; i < this.state.yCoords.length; i++) {
+      if (this.state.currentPlayer === '[Y]') {
+        currRow = this.state.redSpaces.map((pair) => {//collect only x coordinates of each used space of each row
+          if (pair[1] === i) {
+            return pair[0];
+          }
+        })
+          .filter((item) => item !== undefined)
+          .sort((a, b) => a - b);
+      } else if (this.state.currentPlayer === '[R]') {
+        currRow = this.state.yellowSpaces.map((pair) => {
+          if (pair[1] === i) {
+            return pair[0];
+          }
+        })
+          .filter((item) => item !== undefined)
+          .sort((a, b) => a - b);
+      }
+      //console.log(`row ${i}: `, currRow);
+      if (currRow.length > 3) {
+        for (var i = 0; i < 3; i++) {
+
+          if ((currRow[i] && currRow.includes(currRow[i] + 1) && currRow.includes(currRow[i] + 2) && currRow.includes(currRow[i] + 3)) || (currRow[i] === 0 && currRow[1] === 1 && currRow[2] === 2 && currRow[3] === 3)) {
+            console.log('win')
+          }
+        }
+        break;
+      }
+    }
+  }
+
+  checkWin() {
+    this.checkColumnWin();
+    this.checkRowWin();
+  }
+
 
   componentDidMount() {
     this.setState({
@@ -94,62 +134,26 @@ class App extends React.Component {
     return (
       <div className='container row' onClick={(e) => { this.dropPiece(e) }}>
         <div id='column0'>
-          <Square y={5} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={4} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={3} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={2} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={1} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={0} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          {this.state.yCoords.map((coord) => <Square key={`c0${coord}`} y={coord} x={0} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />)}
         </div>
         <div id='column1'>
-          <Square y={5} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={4} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={3} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={2} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={1} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={0} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          {this.state.yCoords.map((coord) => <Square key={`c1${coord}`} y={coord} x={1} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />)}
         </div>
         <div id='column2'>
-          <Square y={5} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={4} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={3} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={2} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={1} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={0} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          {this.state.yCoords.map((coord) => <Square key={`c2${coord}`} y={coord} x={2} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />)}
         </div>
         <div id='column3'>
-          <Square y={5} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={4} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={3} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={2} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={1} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={0} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          {this.state.yCoords.map((coord) => <Square key={`c3${coord}`} y={coord} x={3} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />)}
         </div>
         <div id='column4'>
-          <Square y={5} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={4} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={3} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={2} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={1} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={0} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          {this.state.yCoords.map((coord) => <Square key={`c4${coord}`} y={coord} x={4} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />)}
         </div>
         <div id='column5'>
-          <Square y={5} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={4} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={3} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={2} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={1} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={0} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          {this.state.yCoords.map((coord) => <Square key={`c5${coord}`} y={coord} x={5} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />)}
         </div>
         <div id='column6'>
-          <Square y={5} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={4} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={3} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={2} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={1} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
-          <Square y={0} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />
+          {this.state.yCoords.map((coord) => <Square key={`c6${coord}`} y={coord} x={6} placePiece={this.placePiece} currentPlayer={this.state.currentPlayer} />)}
         </div>
-
       </div>
     )
   }
