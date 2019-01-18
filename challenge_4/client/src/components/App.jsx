b6c70d704d882d9799dcce68c48adf67b0f1882f
akwong372 <akwong372@gmail.com>
2019-01-17 19:11:08 -0800

    Detect win for diagonals

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -5,7 +5,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       currentPlayer: '',
-      yCoords: [0, 1, 2, 3, 4, 5],
+      yCoords: [5, 4, 3, 2, 1, 0],
       xCoords: [0, 1, 2, 3, 4, 5, 6],
       redSpaces: [],
       yellowSpaces: []
@@ -55,8 +55,10 @@ class App extends React.Component {
 
   checkColumnWin() {
     var currColumn = [];
+    var currPlayer = '';
     for (var i = 0; i < this.state.xCoords.length; i++) {
       if (this.state.currentPlayer === '[Y]') {
+        currPlayer = 'Red'
         currColumn = this.state.redSpaces.map((pair) => {//collect only y coordinates of each used space of each column
           if (pair[0] === i) {
             return pair[1];
@@ -65,6 +67,7 @@ class App extends React.Component {
           .filter((item) => item !== undefined)//filter out undefined
           .sort((a, b) => a - b);//sort smallest to largest
       } else if (this.state.currentPlayer === '[R]') {
+        currPlayer = 'Yellow'
         currColumn = this.state.yellowSpaces.map((pair) => {
           if (pair[0] === i) {
             return pair[1];
@@ -73,11 +76,10 @@ class App extends React.Component {
           .filter((item) => item !== undefined)
           .sort((a, b) => a - b);
       }
-      //console.log(`column ${i}: `, currColumn);
       if (currColumn.length > 3) {
         for (var i = 0; i < 2; i++) {
           if ((currColumn[i] && currColumn.includes(currColumn[i] + 1) && currColumn.includes(currColumn[i] + 2) && currColumn.includes(currColumn[i] + 3)) || (currColumn[i] === 0 && currColumn[1] === 1 && currColumn[2] === 2 && currColumn[3] === 3)) {
-            console.log('win')
+            console.log(`${currPlayer} wins`)
           }
         }
         break;
@@ -87,8 +89,10 @@ class App extends React.Component {
 
   checkRowWin() {
     var currRow = [];
+    var currPlayer = '';
     for (var i = 0; i < this.state.yCoords.length; i++) {
       if (this.state.currentPlayer === '[Y]') {
+        currPlayer = 'Red'
         currRow = this.state.redSpaces.map((pair) => {//collect only x coordinates of each used space of each row
           if (pair[1] === i) {
             return pair[0];
@@ -97,6 +101,7 @@ class App extends React.Component {
           .filter((item) => item !== undefined)
           .sort((a, b) => a - b);
       } else if (this.state.currentPlayer === '[R]') {
+        currPlayer = 'Yellow'
         currRow = this.state.yellowSpaces.map((pair) => {
           if (pair[1] === i) {
             return pair[0];
@@ -105,12 +110,11 @@ class App extends React.Component {
           .filter((item) => item !== undefined)
           .sort((a, b) => a - b);
       }
-      //console.log(`row ${i}: `, currRow);
       if (currRow.length > 3) {
         for (var i = 0; i < 3; i++) {
 
           if ((currRow[i] && currRow.includes(currRow[i] + 1) && currRow.includes(currRow[i] + 2) && currRow.includes(currRow[i] + 3)) || (currRow[i] === 0 && currRow[1] === 1 && currRow[2] === 2 && currRow[3] === 3)) {
-            console.log('win')
+            console.log(`${currPlayer} wins`)
           }
         }
         break;
@@ -118,9 +122,45 @@ class App extends React.Component {
     }
   }
 
+  checkDiagWin() {
+    var currDiag = [];
+    var currPlayer = '';
+
+    if (this.state.currentPlayer === '[Y]') {
+      currPlayer = 'Red'
+      currDiag = this.state.redSpaces;
+    } else if (this.state.currentPlayer === '[R]') {
+      currPlayer = 'Yellow'
+      currDiag = this.state.yellowSpaces;
+    }
+
+    if (currDiag.length > 3) {
+      console.log(`${currPlayer}`, currDiag)
+      for (var i = 0; i < currDiag.length; i++) {
+
+        //bottom left to top right
+        if (currDiag.toString().includes([currDiag[i][0], currDiag[i][1]].toString())
+          && currDiag.toString().includes([currDiag[i][0] + 1, currDiag[i][1] + 1].toString())
+          && currDiag.toString().includes([currDiag[i][0] + 2, currDiag[i][1] + 2].toString())
+          && currDiag.toString().includes([currDiag[i][0] + 3, currDiag[i][1] + 3].toString())) {
+          console.log('thing')
+        }
+
+        //bottom right to top left
+        if (currDiag.toString().includes([currDiag[i][0], currDiag[i][1]].toString())
+          && currDiag.toString().includes([currDiag[i][0] - 1, currDiag[i][1] + 1].toString())
+          && currDiag.toString().includes([currDiag[i][0] - 2, currDiag[i][1] + 2].toString())
+          && currDiag.toString().includes([currDiag[i][0] - 3, currDiag[i][1] + 3].toString())) {
+          console.log('thing2')
+        }
+      }
+    }
+  }
+
   checkWin() {
     this.checkColumnWin();
     this.checkRowWin();
+    this.checkDiagWin();
   }
 
 
